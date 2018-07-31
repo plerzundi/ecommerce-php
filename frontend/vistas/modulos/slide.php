16861dad88b4a2256c12ae122e60d950c34d86f1
Patricio Andres Lerzundi Suazo <patriciolerzundi@gmail.com>
2018-07-31 04:12:00 -0400

    Slide
    Creacion slide de productos.

---
+++ frontend/vistas/modulos/slide.php
@@ -0,0 +1,29 @@
+<!--===================================
+            SLIDESHOW
+====================================-->
+
+<div class="container-fluid" id="slide">
+  <div class="row">
+  <!--===================================
+              DIAPOSITIVAS
+  ====================================-->
+  <ul>
+    <li>
+      <img src="http://localhost/backend/vistas/img/slide/default/back_default.jpg">
+      <div class="slideOpciones slideOpcion1">
+        <img class="imgProducto" src="http://localhost/backend/vistas/img/slide/slide1/calzado.png">
+        <div class="textosSlide">
+          <h1>Ejemplo ejemplo</h1>
+          <h2>Ejemplo2 Ejemplo2</h2>
+          <h3>Ejemplo3 Ejemplo 3</h3>
+          <a href="#">
+            <button class="btn btn-default backColor">
+              VER PRODUCTO <span class="fa fa-chevron-right"></span>
+            </button>
+          </a>
+        </div>
+      </div>
+    </li>
+  </ul>
+  </div>
+</div>
