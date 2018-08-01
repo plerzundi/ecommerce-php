510a36ab35f4df10faba777c3a98b6821e8fdff1
Patricio Andres Lerzundi Suazo <patriciolerzundi@gmail.com>
2018-08-01 00:24:19 -0400

    Slide
    Se crea un Slide propio para mostrar los productos.

--- frontend/vistas/modulos/slide.php
+++ frontend/vistas/modulos/slide.php
@@ -8,13 +8,70 @@
               DIAPOSITIVAS
   ====================================-->
   <ul>
+    <!--SLIDE 1-->
     <li>
       <img src="http://localhost/backend/vistas/img/slide/default/back_default.jpg">
       <div class="slideOpciones slideOpcion1">
-        <img class="imgProducto" src="http://localhost/backend/vistas/img/slide/slide1/calzado.png">
-        <div class="textosSlide">
-          <h1>Ejemplo ejemplo</h1>
-          <h2>Ejemplo2 Ejemplo2</h2>
+        <img class="imgProducto" src="http://localhost/backend/vistas/img/slide/slide1/calzado.png" style="top:15%; right:15%; width:45%">
+        <div class="textosSlide" style="top:20%; left:10%; width:40%">
+          <h1 style="color:#333">Ejemplo ejemplo</h1>
+          <h2 style="color:#777">Ejemplo2 Ejemplo2</h2>
+          <h3 style="color:#888;">Ejemplo3 Ejemplo 3</h3>
+          <a href="#">
+            <button class="btn btn-default backColor">
+              VER PRODUCTO <span class="fa fa-chevron-right"></span>
+            </button>
+          </a>
+        </div>
+      </div>
+    </li>
+
+
+    <!--SLIDE 2-->
+    <li>
+      <img src="http://localhost/backend/vistas/img/slide/default/back_default.jpg">
+      <div class="slideOpciones slideOpcion2">
+        <img class="imgProducto" src="http://localhost/backend/vistas/img/slide/slide2/curso.png" style="top:5%; left:15%; width:25%">
+        <div class="textosSlide" style="top:20%; right:10%; width:40%">
+          <h1 style="color: #333">Ejemplo ejemplo</h1>
+          <h2 style="color:#777">Ejemplo2 Ejemplo2</h2>
+          <h3 style="color:#888">Ejemplo3 Ejemplo 3</h3>
+          <a href="#">
+            <button class="btn btn-default backColor">
+              VER PRODUCTO <span class="fa fa-chevron-right"></span>
+            </button>
+          </a>
+        </div>
+      </div>
+    </li>
+
+    <!--SLIDE 3-->
+    <li>
+      <img src="http://localhost/backend/vistas/img/slide/slide3/fondo2.jpg">
+      <div class="slideOpciones slideOpcion2">
+        <img class="imgProducto" src="http://localhost/backend/vistas/img/slide/slide3/iphone.png" style="top:5%; left:15%; width:25%">
+        <div class="textosSlide" style="top:20%; right:10%; width:40%">
+          <h1 style="color:#eee">Ejemplo ejemplo</h1>
+          <h2 style="color:#ccc">Ejemplo2 Ejemplo2</h2>
+          <h3 style="color:#aaa">Ejemplo3 Ejemplo 3</h3>
+          <a href="#">
+            <button class="btn btn-default backColor">
+              VER PRODUCTO <span class="fa fa-chevron-right"></span>
+            </button>
+          </a>
+        </div>
+      </div>
+    </li>
+
+        <!--SLIDE 4-->
+
+    <li>
+      <img src="http://localhost/backend/vistas/img/slide/slide4/fondo3.jpg">
+      <div class="slideOpciones slideOpcion1">
+        <img class="imgProducto" src="" style="top:5%; right:15%; width:25%">
+        <div class="textosSlide" style="top:20%; left:10%; width:40%">
+          <h1 style="color:#333">Ejemplo ejemplo</h1>
+          <h2 style="color:#777">Ejemplo2 Ejemplo2</h2>
           <h3>Ejemplo3 Ejemplo 3</h3>
           <a href="#">
             <button class="btn btn-default backColor">
@@ -25,5 +82,23 @@
       </div>
     </li>
   </ul>
+
+  <!--=========================================
+                  PAGINACIÓN
+  ==========================================-->
+      <ol id="paginacion">
+        <li item="1"><span class="fa fa-circle"></span></li>
+        <li item="2"><span class="fa fa-circle"></span></li>
+        <li item="3"><span class="fa fa-circle"></span></li>
+        <li item="4"><span class="fa fa-circle"></span></li>
+
+      </ol>
+
+    <!--=========================================
+                    FLECHAS
+    ==========================================-->
+    <div class="flechas" id="retroceder"><span class="fa fa-chevron-left"></span></div>
+      <div class="flechas" id="avanzar"><span class="fa fa-chevron-right"></span></div>
+
   </div>
 </div>
